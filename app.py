f782e7bc04b8828b04c49bc9b6a273034e5c1f64
Nikki Klein <63431649+kleinlikecalvin@users.noreply.github.com>
2025-09-08 19:56:51 -0700

    Commits
    - Updates home route
    - Add flashcards template and styles
    - Moves header/footer into their own template and stylesheet
    - Updates styling
    - Implements color palette

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def base():
+def home():
     return render_template("home.html")
+
+
+@app.route("/flashcards")
+def flashcards():
+    return render_template("flashcards.html")
 
 
 if __name__ == "__main__":
